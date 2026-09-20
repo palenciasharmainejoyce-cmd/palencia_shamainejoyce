e4e38566ecd45c43aa9d75223c34a04634bc1895
palenciasharmainejoyce-cmd <palenciasharmainejoyce@isufst.edu.ph>
2026-09-19 22:33:27 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome to my first API!"
+
+
+@app.route('/student')
+def get_student():
+    return jsonify({
+        "student_id": "24-00094",
+        "name": "Sharmaine Joyce Palencia",
+        "program": "BSIT",
+        "year": 3,
+        "section": "B"
+    })
+
+
+@app.route('/hello')
+def say_hello():
+    name = request.args.get('name', 'Student')
+
+    return jsonify({
+        "message": f"Hello, {name}!"
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
